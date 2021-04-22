868a4ecfa1387b002920f101159c59542c6a6b9b
Legend <milad1995honor@gmail.com>
2021-04-22 20:04:21 +0430

    Rename states files

--- store/user.js
+++ store/user.js
@@ -1,4 +1,31 @@
 export const state = () => ({
-	firstName: 'Ashley',
-    lastName: 'Wong'
+	id: '',
+    name: '',
+    tell: '',
+    personal: {
+        avatar: null,
+        province: null,
+        city: null,
+        addr: null,
+        postCode: null,
+    },
+    financial: {
+        shabaa: null,
+        debitCard: {
+            img: null,
+            val: null,
+            validated: 0 
+        },
+        nationalId: {
+            img: null,
+            val: null,
+            validated: 0 
+        },
+        licenseCard: {
+            img: null,
+            val: null,
+            validated: 0 
+        }
+    },
+    scores: null
 })
---
+++ store/wallet.js
@@ -0,0 +1,9 @@
+export const state = () => ({
+    pendingBalanc: 0,
+    availableBalance: 0,
+    withdrawalBalance: 0,
+    transactions: null,
+    expired_at: null,
+
+    // From Membership
+})
