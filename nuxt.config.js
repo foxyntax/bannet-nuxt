32136854cfe5219bfeb2946cfab50ba9874955ea
Legend <milad1995honor@gmail.com>
2021-04-23 21:02:35 +0430

    Add fontawesome to vue head

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/css/animate.css'
+    '~/assets/css/animate.css',
+    '~/assets/css/all.min.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
