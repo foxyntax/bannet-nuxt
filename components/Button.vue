cba510255f0d3ac70b7224befa929fb4846c8328
Legend <milad1995honor@gmail.com>
2021-04-26 22:11:01 +0430

    Add Button.vue

---
+++ components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="bannet-btn" :class="classes">
+    <span v-show="!loading">{{ text }}</span>
+    <div class="spinner" v-show="loading">
+      <div class="dot1"></div>
+      <div class="dot2"></div>
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      required: true,
+      type: String
+    },
+    classes: {
+      required: true,
+      type: String
+    },
+    loading: {
+      required: false,
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
