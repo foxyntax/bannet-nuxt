edef2d259b05c0b3b7b99eb6951e614b82867ac7
Legend <milad1995honor@gmail.com>
2021-05-08 23:47:38 +0430

    Update Footer in logged layout file

--- layouts/logged.vue
+++ layouts/logged.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
+  <div id="logged-container">
     <section id="header"></section>
-    <section id="article"></section>
-    <section id="footer"></section>
+    <Nuxt />
+    <section id="footer">
+      <div class="p-3">
+        <div class="h-9 xs:h-12 grid grid-cols-5 grid-rows-1 gap-1">
+          <div class="text-center col-span-2">
+            <Badge text="خودرو سواری" id="category" icon="fal fa-car text-xl xs:text-3xl" iconPosition="before"/>
+          </div>
+          <div class="text-center foot-icons"><i class="fal fa-sliders-h text-xl xs:text-3xl" /></div>
+          <div class="text-center foot-icons"><i class="fal fa-tire text-xl xs:text-3xl" /></div>
+          <div class="text-center foot-icons"><i class="fal fa-user text-xl xs:text-3xl" /></div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -33,5 +44,23 @@ export default {
 </script>
 
 <style lang="scss">
+#footer {
+  @apply fixed w-full left-0 bottom-0 right-0;
+  border-top: 1px solid lighten($default_color, 70%);
+
+  #category {
+    @apply text-center cursor-pointer py-2 text-xs xs:text-sm #{!important};
+
+    span {
+      @apply relative;
+      top: -5px;
+    }
+  }
+
+  .foot-icons {
+    @apply relative text-default-lighten cursor-pointer;
+    top: 7px;
+  }
+}
 
 </style>
