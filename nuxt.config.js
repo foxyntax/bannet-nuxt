233e542acb7400f57ac6d64d6db1f79693342bed
Legend <milad1995honor@gmail.com>
2021-05-17 13:11:51 +0430

    Change theme-color

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
-      { name: 'theme-color', content: '#24292c' }
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
