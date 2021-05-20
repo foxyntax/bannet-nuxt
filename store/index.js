d618d41f2b1237f40e15f118af6722f81edd676d
Legend <milad1995honor@gmail.com>
2021-05-20 14:04:48 +0430

    Add new mutations

--- store/index.js
+++ store/index.js
@@ -45,6 +45,18 @@ export const mutations = {
 	UPDATE_LAYOUT (state, payload) {
 		state.layout[payload.state] = payload.value
 	},
+	UPDATE_LAYOUT_CATEGORY (state, payload) {
+		state.layout.categories[payload.state] = payload.value
+	},
+	UPDATE_LAYOUT_FILTERS_LIST (state, payload) {
+		state.layout.filters[payload.state].list = payload.value
+	},
+	UPDATE_LAYOUT_FILTERS_CHOSEN (state, payload) {
+		state.layout.filters[payload.state].chosen = payload.value
+	},
+	UPDATE_LAYOUT_REGION (state, payload) {
+		state.layout.regions[payload.state] = payload.value
+	},
 
 	// Tables
 	SET_TABLE (state, payload) {
