e2be4bb42e32ed63f1241d74ba50e450c88ebe36
Legend <milad1995honor@gmail.com>
2021-05-20 14:05:18 +0430

    update new layout states

--- store/layout.js
+++ store/layout.js
@@ -1,6 +1,61 @@
 export const state = () => ({
     isLoading: false,
-    categoryItems: false,
-    filters: false,
-    regionItems: false
+    categories: {
+        enabled: false,
+        chosen: { type: 1, name: 'خودرو سواری', icon: 'fal fa-car' },
+        list: [
+            { type: 1, name: 'خودرو سواری', icon: 'fal fa-car' },
+            { type: 2, name: 'خودرو سنگین', icon: 'fal fa-truck-moving' },
+            { type: 3, name: 'موتورسیکلت', icon: 'fal fa-motorcycle' }
+        ]
+    },
+    regions: {
+        enabled: false,
+        chosen: 'تهران',
+        list: [
+            'تهران',
+            'مشهد',
+            'اصفهان',
+            'تبریز',
+            'شیراز',
+            'ارومیه',
+            'رشت',
+            'بندرعباس',
+            'کیش',
+            'بوشهر',
+            'ساری',
+            'قزوین',
+            'کرج',
+            'قم',
+            'خرم آباد'
+        ]
+    },
+    filters: {
+        brand: {
+            chosen: null,
+            list: ['1', '2', '3', '4', '5', '6']
+        },
+        width: {
+            chosen: null,
+            list: ['1', '2', '3', '4', '5', '6']
+        },
+        weight: {
+            chosen: null,
+            list: ['1', '2', '3', '4', '5', '6']
+        },
+        height: {
+            chosen: null,
+            list: ['1', '2', '3', '4', '5', '6']
+        },
+        tire_height: {
+            chosen: null,
+            list: ['1', '2', '3', '4', '5', '6']
+        },
+        for_back: {
+            chosen: null
+        },
+        for_front: {
+            chosen: null
+        }
+    }
 })
