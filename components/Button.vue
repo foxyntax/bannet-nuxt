ff87c68faa98c062b1093ad32a87cbaf5500fdd9
Legend <milad1995honor@gmail.com>
2021-05-31 13:14:28 +0430

    Add icon feature to button component

--- components/Button.vue
+++ components/Button.vue
@@ -1,9 +1,13 @@
 <template>
   <button class="bannet-btn" :class="classes">
-    <span v-show="!loading">{{ text }}</span>
-    <div class="spinner" v-show="loading">
-      <div class="dot1"></div>
-      <div class="dot2"></div>
+    <span v-show="!loading">
+      <i v-if="iconPosition === 'before'" :class="icon" class="ml-1" />
+      {{ text }}
+      <i v-if="iconPosition === 'after'" :class="icon" class="mr-1" />
+    </span>
+    <div v-show="loading" class="spinner">
+      <div class="dot1" />
+      <div class="dot2" />
     </div>
   </button>
 </template>
@@ -23,6 +27,16 @@ export default {
       required: false,
       type: Boolean,
       default: false
+    },
+    icon: {
+      required: false,
+      type: String,
+      default: ''
+    },
+    iconPosition: {
+        required: false,
+        type: String,
+        default: 'before'
     }
   }
 }
