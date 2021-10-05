ee1495f1f80d12696e0f11cf3eff3dec97f4e2a9
Legend <milad1995honor@gmail.com>
2021-10-05 23:15:34 +0330

    add new component

---
+++ components/Uploader.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="uploader">
+    <label :for="id">
+      <p> {{ title }} </p>
+      <div class="upload-zone">
+        <i class="fas fa-cloud-upload-alt" />
+        <p> {{ placeholder }} </p>
+        <small> {{ size | noticeForSize }} </small>
+      </div>
+    </label>
+    <input
+      :id="id"
+      type="file"
+      :multiple="multiple"
+      :name="value"
+      :required="required"
+      @input="emit"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'SelectInput',
+  filters: {
+    noticeForSize (val) {
+      return `حداکثر حجم قابل قبول ${val} مگابایت است`
+    }
+  },
+	props: {
+    title: {
+			type: String,
+			required: false,
+      default: ''
+		},
+		placeholder: {
+			type: [String, Number, Object],
+			required: false,
+			default: 'برای آپلود فایل، لمس کنید.'
+		},
+    size: {
+			type: Number,
+			required: false,
+			default: 10
+		},
+		multiple: {
+			type: Boolean,
+			required: false,
+			default: false
+		},
+    id: {
+			type: String,
+			required: true
+		},
+		value: {
+			type: [String, Number, Object],
+			required: false,
+			default: null
+		},
+    required: {
+			type: Boolean,
+			required: false,
+			default: false
+		},
+		disabled: {
+			type: Boolean,
+			required: false,
+			default: false
+		}
+	},
+	methods: {
+		emit () {
+			this.$emit('input', this.value)
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
