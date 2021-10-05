1d566b8235ed1c3581afd025bd2524f77fd97271
Legend <milad1995honor@gmail.com>
2021-10-05 23:15:31 +0330

    add new component

---
+++ components/TextInput.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="input-text-area" :class="getSize">
+    <label :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      :name="value"
+      type="text"
+      :placeholder="placeholder"
+      :maxlength="length"
+      :class="styles"
+      :required="required"
+      @input="emit"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'TextInput',
+	props: {
+    size: {
+			type: String,
+			required: false,
+			default: ''
+		},
+		placeholder: {
+			type: String,
+			required: false,
+			default: 'مقدار صحیح را وارد نمایید'
+		},
+    label: {
+			type: String,
+			required: true
+		},
+		id: {
+			type: String,
+			required: true
+		},
+		required: {
+			type: Boolean,
+			required: false,
+			default: false
+		},
+    length: {
+      type: Number,
+      required: false,
+      default: 9999
+    },
+		styles: {
+			type: String,
+			required: false,
+			default: ''
+		},
+    value: {
+      type: [String, Number],
+			required: false,
+			default: ''
+    }
+	},
+  computed: {
+    getSize () {
+      switch (this.size) {
+        case 'full':
+          return 'w-full'
+        case 'half':
+          return 'w-1/2'
+        default:
+          return ''
+      }
+    }
+  },
+	methods: {
+		emit () {
+			this.$emit('input', this.value)
+		}
+	}
+}
+</script>
