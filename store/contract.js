ad6aa813f56c3fb1fdb5cde86465cfd1b810e172
Legend <milad1995honor@gmail.com>
2021-10-11 13:49:52 +0330

    fix comments

--- store/contract.js
+++ store/contract.js
@@ -1,6 +1,7 @@
 export const state = () => ({
     list: null
     /**
+     ** take a note you have to seperate list after fetching data
      ** Example from "meta" col
      * province
      * city
