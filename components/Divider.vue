1dba687fbf262e55e849ccd890b20c684066c11d
Legend <milad1995honor@gmail.com>
2021-10-11 13:52:43 +0330

    add new feature to divider component

--- components/Divider.vue
+++ components/Divider.vue
@@ -6,8 +6,9 @@
 export default {
     props: {
         size: {
-            required: true,
-            type: String
+            required: false,
+            type: String,
+            default: 'normal'
         }
     },
     computed: {
