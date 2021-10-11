045ec4db6856771943eb21b86184ed400a6cea97
Legend <milad1995honor@gmail.com>
2021-10-11 13:51:51 +0330

    remove extra options in collapsed list component

--- components/CollapsedList.vue
+++ components/CollapsedList.vue
@@ -5,15 +5,7 @@
       <i :class="{'fa-angle-down': !expanded, 'fa-angle-up': expanded}" class="fal relative top-0 text-2xl ml-2" />
     </p>
     <ul v-show="expanded" class="w-full overflow-auto divider-xs-b">
-      <li
-        v-for="(item, index) in list"
-        :key="index"
-        class="hover-1 px-4 xs:px-6 py-4 xs:py-6"
-        :class="{'chosen-item': (item === value)}"
-        @click="updateFilterItem(item)"
-      >
-        {{ item }}
-      </li>
+      <slot />
     </ul>
   </div>
 </template>
@@ -24,24 +16,10 @@ export default {
     title: {
       required: true,
       type: String
-    },
-    list: {
-      required: true,
-      type: [Array, Object]
-    },
-    value: {
-      required: false,
-      type: [String, Number, Array, Object],
-      default: ''
     }
   },
   data: () => ({
     expanded: false
-  }),
-  methods: {
-    updateFilterItem (item) {
-      this.$emit('input', item)
-    }
-  }
+  })
 }
 </script>
