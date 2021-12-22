0e107ce1195f674e3985482bdf6fabb6f41f34e2
Legend <milad1995honor@gmail.com>
2021-12-22 13:19:42 +0330

    add auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,49 +1,80 @@
+/* eslint-disable  padded-blocks  */
+
 import Vue from 'vue'
 
-export default function ({ route, $axios, redirect, store }) {
-	// First, is public route
-	const isPublicRoute = route.fullPath.includes('/order/new')
+/**
+ * Settings
+ */
+const failedRedirection = '/'
+const loginRoute = ['/welcome']
+const PublicRoutes = {
+  const: ['/'],
+  exp: ['/tyre']
+}
 
-	// Logout Automaticlly if we navigate to loggin page
-	if (route.fullPath === '/login' || route.fullPath === '/login/') {
-		Vue.prototype.$cookie.delete('logged')
-		$axios.defaults.headers.common.Authorization = null
-		store.commit('UPDATE_USER_ACTIVE', { index: 'id', value: 0 })
-		store.commit('UPDATE_USER_ACTIVE', { index: 'tell', value: '' })
+export default function ({ route, $axios, redirect, store }) {
+
+  // Get token name from cookie
+  const tokenName = Vue.prototype.$cookie.get('webtoken') ?? null
+
+  // Logout Automaticlly if we navigate to loggin page
+  const isLoginRoute = (loginRoute.filter(item => route.fullPath.includes(item)).length > 0)
+
+  // Reject if we have different ID in URL
+  const hasSavedID = (typeof store.state.user.id !== 'undefined' && store.state.user.id !== 0)
+  const isIDinURL = (typeof route.params.id !== 'undefined')
+
+  // Define if there is Auth Value in header and cookie
+  const isAuthHeaderEnabled = (typeof $axios.defaults.headers.common.Authorization !== 'undefined')
+  const isAuthCookieEnabled = (tokenName !== null)
+
+  /**
+  ** Public routes will be ignored redireciton, if auth progress is failed.
+  */
+  const isPublicRoute = (
+    PublicRoutes.const.filter(item => route.fullPath === item).length > 0 ||
+    PublicRoutes.exp.filter(item => route.fullPath.includes(item)).length > 0
+  )
+
+  // Reset all user data if we wanna go to login page
+	if (isLoginRoute) {
+		store.dispatch('reset_user_data')
 		return
 	}
 
-	// Get All of Auth Values
-	const authInHeader = (typeof $axios.defaults.headers.common.Authorization !== 'undefined')
-		? $axios.defaults.headers.common.Authorization
-		: false
+  // Reset all user data if we have different ids [it's useful for situtation that some scripts try to change id of routes]
+  // eslint-disable-next-line eqeqeq
+  if (hasSavedID && isIDinURL && (store.state.user.id != route.params.id)) {
+    console.log('%s reset cause of defferent ids', 'color: red; font-size: 12px; padding: 20px; height: 80px')
+    store.dispatch('reset_user_data')
+    return redirect(failedRedirection)
+  }
 
-	const hasID = (typeof store.state.user.id !== 'undefined' && store.state.user.id !== 0)
+  // Get user data or stay in page or even redirect to a public page
+  if (isAuthCookieEnabled && isAuthHeaderEnabled) {
 
-	const authInCookie = (Vue.prototype.$cookie.get('logged') !== null)
-		? Vue.prototype.$cookie.get('logged')
-		: false
+    Vue.prototype.$cookie.set('webtoken', tokenName, { expires: '1Y' })
 
-	if (authInCookie !== false) { // Check Verification if Cookie Setted Before
-		if (authInHeader !== false && hasID !== false) { // everything is okay, go ahead
-			Vue.prototype.$cookie.set('logged', authInCookie, { expires: '1Y' })
-		} else { // Authorization or vuex states lost
-			$axios.post('api-token/check', { name: authInCookie }).then((res) => {
-				// Renew API token
-				$axios.defaults.headers.common.Authorization = 'Bearer ' + res.data.token
-				Vue.prototype.$cookie.set('logged', res.data.token_name, { expires: '1Y' })
+  } else if (isAuthCookieEnabled) {
 
-				// Fetch User Data
-				const user = res.data.user
-				store.dispatch('fetch_user', user)
-			}).catch(() => {
-				// console.log(err.response.data)
-				return redirect('/login')
-			})
-		}
-	} else if (!authInCookie && authInHeader !== false && hasID !== false) { // Cookie Deleted, but Auth is Succeed
-		Vue.prototype.$cookie.set('logged', store.state.user.tell, { expires: '1Y' })
-	} else if ((!authInCookie && (!authInHeader || !hasID)) && !isPublicRoute) { // Push to Login Page if Auth is Failed or RoutPath is not public
-		return redirect('/login')
-	}
+    store.dispatch('api_check', {
+      name: 'name',
+      value: tokenName
+    })
+
+  } else if (isAuthHeaderEnabled) {
+
+    const token = $axios.defaults.headers.common.Authorization.replace('Bearer ', '')
+    store.dispatch('api_check', {
+      name: 'token',
+      value: token
+    })
+
+  } else if (!isAuthCookieEnabled && !isAuthHeaderEnabled) {
+    if (isPublicRoute) { return }
+
+    // we are in private route and we must redirect user to public user that we had defined before
+    console.log('%s reset cause of vanished tokens', 'color: red; font-size: 12px; padding: 20px; height: 80px')
+    return redirect(failedRedirection)
+  }
 }
